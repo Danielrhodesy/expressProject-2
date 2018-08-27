7e3c50426dd908499d2d1070c554fef8cad1fa7f
danielrhodesy <danielr@outlook.co.nz>
2018-08-28 08:48:03 +1200

    Latest

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+var app = express()
+const router = express.Router()
+
+const home = require('./routes/index');
+const about = require('./routes/about');
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}))
+
+app.set('view engine', 'handlebars')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.static())
+
+app.use('/', home)
+app.use('/about', about)
+
+app.set('port', (process.env.PORT || 3000))
+app.listen(app.get('port'), function () {
+  console.log('Server')
+})
